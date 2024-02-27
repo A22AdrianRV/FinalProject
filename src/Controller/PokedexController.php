f6182a18418c2494d5c38d35db530a518806a168
A22AdrianRV <A22AdrianRV@iessanclemente.net>
2024-02-27 14:25:15 +0100

    First commit

---
+++ src/Controller/PokedexController.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Controller;
+use App\Entity\Pokedex;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+    class PokedexController extends AbstractController{
+
+
+        #[Route('/pokemon/add')]
+        public function InsertPokemon(HttpClientInterface $http,EntityManagerInterface $entityManagerInterface):Response{
+
+            $response = $http->request('GET',"https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0",[
+                'headers' => [
+                    'Accept' => 'applications/json',
+                ],
+            ]);
+
+            $content = json_decode($response->getContent());
+            foreach($content->results as $pokemon){
+                $resp = $http->request('GET',$pokemon->url);
+                $pokemonInfo = json_decode($resp->getContent());
+                $poke = new Pokedex();
+                $poke->setName($pokemonInfo->name);
+                // $poke->setType1($pokemonInfo->types[0]->type->name);
+                // $poke->setType2(count($pokemonInfo->types) > 1 ? $pokemonInfo->types[1]->type->name : null);
+                $poke->setTypes([$pokemonInfo->types[0]->type->name,count($pokemonInfo->types) > 1 ? $pokemonInfo->types[1]->type->name : null]);
+                // $poke->setAbility1($pokemonInfo->abilities[0]->ability->name);
+                // $poke->setAbility2( (count($pokemonInfo->abilities) == 1) ? null  :($pokemonInfo->abilities[1]->is_hidden ? null: $pokemonInfo->abilities[1]->ability->name));
+                // $poke->setHiddenAbility((count($pokemonInfo->abilities) == 1) ? null:($pokemonInfo->abilities[1]->is_hidden ? $pokemonInfo->abilities[1]->ability->name : (count($pokemonInfo->abilities) > 2 ? $pokemonInfo->abilities[2]->ability->name:null)));
+                $AbilityArr = [];
+                foreach($pokemonInfo->abilities as $ability){
+                    $AbilityJSON = [
+                        "name" => $ability->ability->name,
+                        "is_hidden" => $ability->is_hidden,
+                    ];
+                    array_push($AbilityArr,$AbilityJSON);
+                }
+                $poke->setAbilities([]);
+                $arr = [];
+                foreach($pokemonInfo->moves as $movement){
+                    $moveJSON = [
+                        $movement->move->name => [
+                            "learn_method" => $movement->version_group_details[0]->move_learn_method->name,
+                            "level_learned_at" => $movement->version_group_details[0]->level_learned_at,
+                        ]];
+
+                    array_push($arr,$moveJSON);
+                    
+                }
+                $poke->setMoves($arr);
+
+                $poke->setImages([
+                    "front" =>$pokemonInfo->sprites->front_default,
+                    "back"=>$pokemonInfo->sprites->back_default,
+                    "front_shiny"=>$pokemonInfo->sprites->front_shiny,
+                    "back_shiny"=>$pokemonInfo->sprites->back_shiny,
+                ]);
+            
+                $poke->setStats([
+                    "HP" => $pokemonInfo->stats[0]->base_stat,
+                    "Attack" =>$pokemonInfo->stats[1]->base_stat,
+                    "Defense"=>$pokemonInfo->stats[2]->base_stat,
+                    "Special Attack"=>$pokemonInfo->stats[3]->base_stat,
+                    "Special Defense"=>$pokemonInfo->stats[4]->base_stat,
+                    "Speed"=>$pokemonInfo->stats[5]->base_stat,
+                    "BST" => $pokemonInfo->stats[0]->base_stat + $pokemonInfo->stats[1]->base_stat + $pokemonInfo->stats[2]->base_stat + $pokemonInfo->stats[3]->base_stat +
+                $pokemonInfo->stats[4]->base_stat + $pokemonInfo->stats[5]->base_stat,
+                ]);
+                $species = $http->request('GET',$pokemonInfo->species->url);
+                $GetEvoChain = $http->request('GET',json_decode($species->getContent())->evolution_chain->url);
+                $evoChain = json_decode($GetEvoChain->getContent());
+                $poke->setEvolutionChain(json_decode(json_encode($evoChain->chain),true));
+                $entityManagerInterface->persist($poke);
+                $entityManagerInterface->flush();
+                
+        }
+        return new Response(sprintf("Todos los pokes añadidos"));
+
+    }
+    
+
+}
+
+
+?>
---
+++ src/Entity/Pokedex.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokedexRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokedexRepository::class)]
+class Pokedex
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $types = [];
+
+    #[ORM\Column]
+    private array $abilities = [];
+
+    #[ORM\Column]
+    private array $moves = [];
+
+
+
+    #[ORM\Column]
+    private array $stats = [];
+
+    #[ORM\Column]
+    private array $evolution_chain = [];
+
+    #[ORM\Column]
+    private array $images = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTypes(): array
+    {
+        return $this->types;
+    }
+
+    public function setTypes(array $types): static
+    {
+        $this->types = $types;
+
+        return $this;
+    }
+
+    public function getAbilities(): array
+    {
+        return $this->abilities;
+    }
+
+    public function setAbilities(array $abilities): static
+    {
+        $this->abilities = $abilities;
+
+        return $this;
+    }
+
+    public function getMoves(): array
+    {
+        return $this->moves;
+    }
+
+    public function setMoves(array $moves): static
+    {
+        $this->moves = $moves;
+
+        return $this;
+    }
+
+
+    public function getStats(): array
+    {
+        return $this->stats;
+    }
+
+    public function setStats(array $stats): static
+    {
+        $this->stats = $stats;
+
+        return $this;
+    }
+
+    public function getEvolutionChain(): array
+    {
+        return $this->evolution_chain;
+    }
+
+    public function setEvolutionChain(array $evolution_chain): static
+    {
+        $this->evolution_chain = $evolution_chain;
+
+        return $this;
+    }
+
+    public function getImages(): array
+    {
+        return $this->images;
+    }
+
+    public function setImages(array $images): static
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+}
